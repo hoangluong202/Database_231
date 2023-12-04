577c6f774f7f600cf897a041f308bea5d2f2a5cf
hoangluong202 <hl01012002@gmail.com>
2023-12-04 16:11:38 +0700

    generate file migration sql

---
+++ prisma/migrations/20231204085518_init/migration.sql
@@ -0,0 +1,270 @@
+-- CreateEnum
+CREATE TYPE "CourseLabel" AS ENUM ('MATH');
+
+-- CreateEnum
+CREATE TYPE "AudienceLabel" AS ENUM ('ELEMENTARY');
+
+-- CreateEnum
+CREATE TYPE "MaterialType" AS ENUM ('VIDEO', 'TEXT', 'AUDIO');
+
+-- CreateEnum
+CREATE TYPE "AnswerOption" AS ENUM ('A', 'B', 'C', 'D');
+
+-- CreateEnum
+CREATE TYPE "PaymentMethod" AS ENUM ('BANK_TRANSFER', 'CASH');
+
+-- CreateEnum
+CREATE TYPE "Rating" AS ENUM ('ONE', 'TWO', 'THREE', 'FOUR', 'FIVE');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" VARCHAR(50) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "firstName" VARCHAR(50) NOT NULL,
+    "lastName" VARCHAR(50) NOT NULL,
+    "avatarUrl" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Student" (
+    "userId" INTEGER NOT NULL,
+    "target" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "Student_pkey" PRIMARY KEY ("userId")
+);
+
+-- CreateTable
+CREATE TABLE "Instructor" (
+    "userId" INTEGER NOT NULL,
+    "bankAccountNumber" VARCHAR(20) NOT NULL,
+    "position" VARCHAR(100) NOT NULL,
+
+    CONSTRAINT "Instructor_pkey" PRIMARY KEY ("userId")
+);
+
+-- CreateTable
+CREATE TABLE "Course" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+    "courseLabel" "CourseLabel" NOT NULL,
+    "audienceLabel" "AudienceLabel" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "totalDuration" INTEGER NOT NULL,
+    "totalSections" INTEGER NOT NULL,
+    "instructorId" INTEGER NOT NULL,
+
+    CONSTRAINT "Course_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Certificate" (
+    "id" SERIAL NOT NULL,
+    "content" TEXT NOT NULL,
+    "expirationDate" TIMESTAMP(3) NOT NULL,
+    "courseId" INTEGER NOT NULL,
+
+    CONSTRAINT "Certificate_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FreeCourse" (
+    "courseId" INTEGER NOT NULL,
+    "sponsorName" VARCHAR(100) NOT NULL,
+
+    CONSTRAINT "FreeCourse_pkey" PRIMARY KEY ("courseId")
+);
+
+-- CreateTable
+CREATE TABLE "PaidCourse" (
+    "courseId" INTEGER NOT NULL,
+    "priceOriginal" INTEGER NOT NULL,
+    "priceDiscounted" INTEGER NOT NULL,
+    "discountPercentage" INTEGER NOT NULL,
+    "promoEndDate" TIMESTAMP(3) NOT NULL,
+    "parentId" INTEGER,
+
+    CONSTRAINT "PaidCourse_pkey" PRIMARY KEY ("courseId")
+);
+
+-- CreateTable
+CREATE TABLE "Section" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "totalCompletionTime" INTEGER NOT NULL,
+    "totalLectures" INTEGER NOT NULL,
+    "courseId" INTEGER NOT NULL,
+
+    CONSTRAINT "Section_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Lecture" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+    "duration" INTEGER NOT NULL,
+    "sectionId" INTEGER NOT NULL,
+
+    CONSTRAINT "Lecture_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Material" (
+    "lectureId" INTEGER NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "url" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "Material_pkey" PRIMARY KEY ("lectureId")
+);
+
+-- CreateTable
+CREATE TABLE "Quiz" (
+    "lectureId" INTEGER NOT NULL,
+    "totalQuestions" INTEGER NOT NULL,
+
+    CONSTRAINT "Quiz_pkey" PRIMARY KEY ("lectureId")
+);
+
+-- CreateTable
+CREATE TABLE "Question" (
+    "id" SERIAL NOT NULL,
+    "content" TEXT NOT NULL,
+    "correctOption" "AnswerOption" NOT NULL,
+    "quizId" INTEGER NOT NULL,
+
+    CONSTRAINT "Question_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" SERIAL NOT NULL,
+    "totalCost" INTEGER NOT NULL,
+    "paymentMethod" "PaymentMethod" NOT NULL,
+    "studentId" INTEGER NOT NULL,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "name" VARCHAR(100) NOT NULL,
+    "content" TEXT NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+    "courseId" INTEGER NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("name","courseId")
+);
+
+-- CreateTable
+CREATE TABLE "StudentStudyLecture" (
+    "studentId" INTEGER NOT NULL,
+    "lectureId" INTEGER NOT NULL,
+
+    CONSTRAINT "StudentStudyLecture_pkey" PRIMARY KEY ("studentId","lectureId")
+);
+
+-- CreateTable
+CREATE TABLE "StudentReviewCourse" (
+    "studentId" INTEGER NOT NULL,
+    "courseId" INTEGER NOT NULL,
+    "rating" "Rating" NOT NULL,
+    "content" TEXT NOT NULL,
+    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "StudentReviewCourse_pkey" PRIMARY KEY ("studentId","courseId")
+);
+
+-- CreateTable
+CREATE TABLE "StudentRegisterFreeCourse" (
+    "studentId" INTEGER NOT NULL,
+    "courseId" INTEGER NOT NULL,
+
+    CONSTRAINT "StudentRegisterFreeCourse_pkey" PRIMARY KEY ("studentId","courseId")
+);
+
+-- CreateTable
+CREATE TABLE "Answer" (
+    "questionId" INTEGER NOT NULL,
+    "answerOption" "AnswerOption" NOT NULL,
+    "content" TEXT NOT NULL,
+    "explanation" TEXT NOT NULL,
+
+    CONSTRAINT "Answer_pkey" PRIMARY KEY ("questionId","answerOption")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Instructor_bankAccountNumber_key" ON "Instructor"("bankAccountNumber");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Certificate_courseId_key" ON "Certificate"("courseId");
+
+-- AddForeignKey
+ALTER TABLE "Student" ADD CONSTRAINT "Student_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Instructor" ADD CONSTRAINT "Instructor_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Course" ADD CONSTRAINT "Course_instructorId_fkey" FOREIGN KEY ("instructorId") REFERENCES "Instructor"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Certificate" ADD CONSTRAINT "Certificate_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FreeCourse" ADD CONSTRAINT "FreeCourse_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PaidCourse" ADD CONSTRAINT "PaidCourse_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PaidCourse" ADD CONSTRAINT "PaidCourse_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "PaidCourse"("courseId") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Section" ADD CONSTRAINT "Section_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Lecture" ADD CONSTRAINT "Lecture_sectionId_fkey" FOREIGN KEY ("sectionId") REFERENCES "Section"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Material" ADD CONSTRAINT "Material_lectureId_fkey" FOREIGN KEY ("lectureId") REFERENCES "Lecture"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Quiz" ADD CONSTRAINT "Quiz_lectureId_fkey" FOREIGN KEY ("lectureId") REFERENCES "Lecture"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Question" ADD CONSTRAINT "Question_quizId_fkey" FOREIGN KEY ("quizId") REFERENCES "Quiz"("lectureId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "Student"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Category" ADD CONSTRAINT "Category_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "StudentStudyLecture" ADD CONSTRAINT "StudentStudyLecture_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "Student"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "StudentStudyLecture" ADD CONSTRAINT "StudentStudyLecture_lectureId_fkey" FOREIGN KEY ("lectureId") REFERENCES "Lecture"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "StudentReviewCourse" ADD CONSTRAINT "StudentReviewCourse_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "Student"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "StudentReviewCourse" ADD CONSTRAINT "StudentReviewCourse_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "StudentRegisterFreeCourse" ADD CONSTRAINT "StudentRegisterFreeCourse_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "Student"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "StudentRegisterFreeCourse" ADD CONSTRAINT "StudentRegisterFreeCourse_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Answer" ADD CONSTRAINT "Answer_questionId_fkey" FOREIGN KEY ("questionId") REFERENCES "Question"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
