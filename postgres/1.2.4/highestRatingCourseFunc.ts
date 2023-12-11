ce374d96cb459e87686a4c04ae5ebd0cd2bf5423
hoangluong202 <hl01012002@gmail.com>
2023-12-11 18:14:49 +0700

    feat(1.2.4): separete file of each function in 1.2.4

--- postgres/1.2.4/highestRatingCourseFunc.ts
+++ postgres/1.2.4/highestRatingCourseFunc.ts
@@ -2,7 +2,7 @@ import { AudienceLabel } from '@prisma/client';
 import { Pool } from 'pg';
 const pool = new Pool({ connectionString: 'postgresql://postgres:password@localhost:5432/postgres' });
 
-async function addFunction() {
+async function highestRatingCourseFunc() {
     //1.2.4.1
     await pool.query(`
     CREATE OR REPLACE FUNCTION get_highest_rating_course(audience_label "AudienceLabel") RETURNS TABLE("name" VARCHAR, "averageRating" NUMERIC(10, 2)) AS $$
@@ -45,42 +45,7 @@ async function addFunction() {
     $$ LANGUAGE PLPGSQL;
   `);
 
-    //1.2.4.2
-    await pool.query(`
-    CREATE OR REPLACE FUNCTION get_revenue(YEAR INT) RETURNS TABLE(name VARCHAR, revenue INT) AS $$
-    DECLARE
-      pai_cou RECORD;
-      total_revenue INT;
-    BEGIN
-      IF year IS NULL THEN
-        RAISE EXCEPTION 'Year is null!';
-      END IF;
-
-      IF year < 0 THEN
-        RAISE EXCEPTION 'Year is invalid!';
-      END IF;
-
-      FOR pai_cou IN
-        SELECT "Course".id, "Course".name, "PaidCourse"."priceDiscounted"
-        FROM "PaidCourse"
-        LEFT JOIN "Course" ON "Course".id = "PaidCourse"."courseId"
-      LOOP
-        SELECT COALESCE(SUM("PaidCourse"."priceDiscounted"),0) INTO total_revenue
-        FROM "PaidCourse"
-        JOIN "PaidCourseOrder" ON "PaidCourseOrder"."paidCourseId" = "PaidCourse"."courseId"
-        JOIN "Order" ON "Order".id = "PaidCourseOrder"."orderId"
-        WHERE "PaidCourse"."courseId" = pai_cou.id AND EXTRACT(YEAR FROM "Order"."createdAt") = year;
-
-        name := pai_cou.name;
-        revenue := total_revenue;
-        RETURN NEXT;
-      END LOOP;
-    END;
-    $$ LANGUAGE PLPGSQL;
-    `);
-
     // Test
-    //1.2.4.1
     const audienceLabel = AudienceLabel.Beginner;
     const res1 = await pool.query(
         `
@@ -89,14 +54,6 @@ async function addFunction() {
         [audienceLabel]
     );
     console.log('Result of highest rating course:\n', res1.rows);
-    //1.2.4.2
-    const year = 2023;
-    const res2 = await pool.query(
-        `
-    SELECT * FROM get_revenue($1);
-    `,
-        [year]
-    );
-    console.log('Resule of revenue of paid course:\n', res2.rows);
+    process.exit(0);
 }
-addFunction();
+highestRatingCourseFunc();
