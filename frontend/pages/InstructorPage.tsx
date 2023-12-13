284e3917da6995dbed5d7fc19c576a7cbb25d7ed
ngyngcphu <nguyenngocphu225@gmail.com>
2023-12-14 05:36:58 +0700

    feat(filter-and-sort): connect api filter and sort

--- frontend/pages/InstructorPage.tsx
+++ frontend/pages/InstructorPage.tsx
@@ -1,59 +1,85 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import moment from 'moment';
-import { Card, CardHeader, CardBody, Chip, Input, Rating, Typography } from '@material-tailwind/react';
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import {
+    Button,
+    Card,
+    CardHeader,
+    CardBody,
+    Chip,
+    Input,
+    Rating,
+    Popover,
+    PopoverContent,
+    PopoverHandler,
+    Typography
+} from '@material-tailwind/react';
+import { ChevronUpIcon, FunnelIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { createColumnHelper, flexRender, getCoreRowModel, RowModel, useReactTable } from '@tanstack/react-table';
-import { FilterProject } from '@fe/components';
-import { COURSE_LABEL_COLOR, AUDIENCE_LABEL_COLOR } from '@fe/constants';
+import filter from '@fe/assets/filter.svg';
+import { COURSE_LABEL_COLOR, AUDIENCE_LABEL_COLOR, COURSE_LABEL, AUDIENCE_LABEL } from '@fe/constants';
+import { useReviewStore } from '@fe/states';
 import { formatSecondsToHourMinute } from '@fe/utils';
 
 export function InstructorPage() {
-    const mockData = [
-        {
-            courseId: 1,
-            courseName: 'HTML for Beginners',
-            courseLabel: 'HighestRated' as CourseLabel,
-            audienceLabel: 'Beginner' as AudienceLabel,
-            updatedAt: '1/1/2024',
-            totalDuration: 2316603,
-            sponsorName: 'Google',
-            priceDiscounted: null,
-            averageRating: 4.7
-        },
-        {
-            courseId: 1,
-            courseName: 'HTML for Beginners',
-            courseLabel: 'HighestRated' as CourseLabel,
-            audienceLabel: 'Beginner' as AudienceLabel,
-            updatedAt: '1/1/2024',
-            totalDuration: 2316603,
-            sponsorName: 'Google',
-            priceDiscounted: null,
-            averageRating: 4.7
-        },
-        {
-            courseId: 1,
-            courseName: 'HTML for Beginners',
-            courseLabel: 'HighestRated' as CourseLabel,
-            audienceLabel: 'Beginner' as AudienceLabel,
-            updatedAt: '1/1/2024',
-            totalDuration: 2316603,
-            sponsorName: 'Google',
-            priceDiscounted: null,
-            averageRating: 4.7
-        },
-        {
-            courseId: 1,
-            courseName: 'HTML for Beginners',
-            courseLabel: 'HighestRated' as CourseLabel,
-            audienceLabel: 'Beginner' as AudienceLabel,
-            updatedAt: '1/1/2024',
-            totalDuration: 2316603,
-            sponsorName: 'Google',
-            priceDiscounted: null,
-            averageRating: 4.7
-        }
-    ];
+    const { listInstructorCourses, getListCoursesByInstructorId /*filterAndSortCourses*/ } = useReviewStore();
+    const [rotate, setRotate] = useState<number[]>([0, 0, 0, 0, 0]);
+    const [filterAndSortPayload, setFilterAndSortPayload] = useState<FilterAndSortPayload>({
+        courseLabels: null,
+        audiencelabels: null,
+        sponsorName: null,
+        sortColumns: ['updatedAt'],
+        sortOrders: ['DESC'],
+        minAverageRating: null
+    });
+
+    const handleCourseLabels = (courseLabel: string) => {
+        setFilterAndSortPayload((prevFilterAndSortPayload) => {
+            const idx = (prevFilterAndSortPayload.courseLabels || []).findIndex((item) => item === courseLabel);
+
+            if (idx !== -1) {
+                const newCourseLabels = (prevFilterAndSortPayload.courseLabels || []).slice();
+                newCourseLabels.splice(idx, 1);
+                return {
+                    ...prevFilterAndSortPayload,
+                    courseLabels: newCourseLabels.length > 0 ? newCourseLabels : null
+                };
+            } else {
+                const newCourseLabels = [...(prevFilterAndSortPayload.courseLabels || []), courseLabel];
+                return {
+                    ...prevFilterAndSortPayload,
+                    courseLabels: newCourseLabels
+                };
+            }
+        });
+    };
+
+    // const handleAudienceLabels = (audienceLabel: string) => {
+    //     setFilterAndSortPayload((prevFilterAndSortPayload) => {
+    //         const idx = (prevFilterAndSortPayload.courseLabels || []).findIndex(
+    //             (item) => item === audienceLabel
+    //         );
+
+    //         if (idx !== -1) {
+    //             const newCourseLabels = (prevFilterAndSortPayload.courseLabels || []).slice();
+    //             newCourseLabels.splice(idx, 1);
+    //             return {
+    //                 ...prevFilterAndSortPayload,
+    //                 courseLabels: newCourseLabels.length > 0 ? newCourseLabels : null,
+    //             };
+    //         } else {
+    //             const newCourseLabels = [...(prevFilterAndSortPayload.courseLabels || []), audienceLabel];
+    //             return {
+    //                 ...prevFilterAndSortPayload,
+    //                 courseLabels: newCourseLabels,
+    //             };
+    //         }
+    //     });
+    // };
+
+    useEffect(() => {
+        // filterAndSortCourses(2, filterAndSortPayload);
+        getListCoursesByInstructorId(2);
+    }, [getListCoursesByInstructorId]);
 
     const columnHelper = createColumnHelper<InstructorCourse>();
 
@@ -70,7 +96,7 @@ export function InstructorPage() {
             columnHelper.accessor('courseLabel', {
                 header: 'NHÃN',
                 cell: (info) => (
-                    <div className='w-max flex items-center gap-1'>
+                    <div className='w-max flex flex-col gap-1'>
                         <Chip
                             variant='ghost'
                             size='sm'
@@ -89,7 +115,7 @@ export function InstructorPage() {
                 )
             }),
             columnHelper.accessor('totalDuration', {
-                header: 'THỜI LƯỢNG KHÓA HỌC',
+                header: 'THỜI LƯỢNG',
                 cell: (info) => <Typography placeholder=''>{formatSecondsToHourMinute(info.getValue())}</Typography>
             }),
             columnHelper.accessor('priceDiscounted', {
@@ -98,7 +124,7 @@ export function InstructorPage() {
                     info.getValue() !== null ? (
                         <Chip color='amber' value={`${(info.getValue() ?? 0).toLocaleString('en-US')} VNĐ`} className='w-fit' />
                     ) : (
-                        <div className='flex items-center justify-between'>
+                        <div className='flex flex-col gap-1'>
                             <Chip color='teal' value='Miễn phí' className='w-fit normal-case' />
                             <Chip variant='ghost' value={`Được tài trợ bởi ${info.row.original.sponsorName}`} className='normal-case' />
                         </div>
@@ -108,8 +134,8 @@ export function InstructorPage() {
                 header: 'ĐIỂM RATING',
                 cell: (info) => (
                     <div className='flex flex-col items-center gap-2 font-bold text-blue-gray-500'>
-                        {info.getValue()}
-                        <Rating placeholder='' value={Math.floor(info.getValue())} readonly />
+                        {Number(info.getValue()).toFixed(1)}
+                        <Rating placeholder='' value={Math.floor(Number(Number(info.getValue()).toFixed(1)))} readonly />
                     </div>
                 )
             }),
@@ -128,9 +154,83 @@ export function InstructorPage() {
 
     const fileTable = useReactTable<InstructorCourse>({
         columns: columnDefs,
-        data: mockData ?? [],
+        data: listInstructorCourses ?? [],
         getCoreRowModel: getCoreRowModel<RowModel<InstructorCourse>>()
     });
+
+    const FilterProject = () => {
+        return (
+            <Popover placement='bottom-start'>
+                <PopoverHandler>
+                    <Chip
+                        variant='ghost'
+                        color='teal'
+                        className='cursor-pointer hover:bg-teal-100 p-2'
+                        value={
+                            <div className='flex items-center gap-1'>
+                                <FunnelIcon color='teal' className='w-6 h-6' />
+                                <Typography placeholder='' variant='small' color='teal' className='font-bold normal-case'>
+                                    Lọc khóa học
+                                </Typography>
+                            </div>
+                        }
+                    />
+                </PopoverHandler>
+                <PopoverContent placeholder='' className='grid gap-2 w-76'>
+                    <div className='grid gap-2'>
+                        <Typography placeholder='' variant='h6' color='blue-gray'>
+                            Nhãn khóa học
+                        </Typography>
+                        <div className='flex gap-1 justify-items-center'>
+                            {COURSE_LABEL.map((item, index) => (
+                                <Button
+                                    key={index}
+                                    tabIndex={-1}
+                                    placeholder=''
+                                    variant='outlined'
+                                    size='sm'
+                                    color={COURSE_LABEL_COLOR[item as CourseLabel]}
+                                    className={
+                                        'rounded-full focus:ring-0 hover:bg-gray-200 normal-case w-fit' +
+                                        ((filterAndSortPayload.courseLabels ?? []).includes(item) ? ' bg-teal-100' : '')
+                                    }
+                                    onClick={() => async () => {
+                                        handleCourseLabels(item);
+                                    }}
+                                >
+                                    {item}
+                                </Button>
+                            ))}
+                        </div>
+                    </div>
+                    <div className='grid gap-2'>
+                        <Typography placeholder='' variant='h6' color='blue-gray'>
+                            Nhãn đối tượng
+                        </Typography>
+                        <div className='flex gap-1 justify-items-center'>
+                            {AUDIENCE_LABEL.map((item, index) => (
+                                <Button
+                                    key={index}
+                                    tabIndex={-1}
+                                    placeholder=''
+                                    variant='outlined'
+                                    size='sm'
+                                    color={AUDIENCE_LABEL_COLOR[item as AudienceLabel]}
+                                    className='rounded-full focus:ring-0 hover:bg-gray-200 w-fit'
+                                >
+                                    {item}
+                                </Button>
+                            ))}
+                        </div>
+                    </div>
+                    <div className='flex items-center gap-2 p-1 w-fit cursor-pointer hover:bg-red-100 hover:rounded-full hover:border hover:border-red-200 active:border-red-300 active:bg-red-200'>
+                        <img src={filter} alt='Funnel Slash' />
+                        <span className='text-red-500 text-base font-medium'>Bỏ lựa chọn</span>
+                    </div>
+                </PopoverContent>
+            </Popover>
+        );
+    };
 
     return (
         <Card placeholder='' className='h-full w-full'>
@@ -152,20 +252,33 @@ export function InstructorPage() {
                     <thead>
                         {fileTable.getHeaderGroups().map((headerGroup) => (
                             <tr key={headerGroup.id} className='bg-gray-100'>
-                                {headerGroup.headers.map((header) => (
+                                {headerGroup.headers.map((header, index) => (
                                     <th
                                         key={header.id}
                                         colSpan={header.colSpan}
                                         className='border-b border-blue-gray-100 bg-gray/2 cursor-pointer border-y bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50'
+                                        onClick={() =>
+                                            setRotate((prevRotate) => {
+                                                const newRotate = [...prevRotate];
+                                                newRotate[index] = newRotate[index] === 0 ? 180 : 0;
+                                                return newRotate;
+                                            })
+                                        }
                                     >
                                         {header.isPlaceholder ? null : (
                                             <Typography
                                                 placeholder=''
                                                 variant='small'
                                                 color='blue-gray'
-                                                className='font-semibold leading-none opacity-70'
+                                                className='flex items-center justify-between font-semibold leading-none opacity-70'
                                             >
                                                 {flexRender(header.column.columnDef.header, header.getContext()) ?? ''}
+                                                {index !== 1 && (
+                                                    <ChevronUpIcon
+                                                        strokeWidth={2}
+                                                        className={`h-4 w-4 transform rotate-${rotate[index]}`}
+                                                    />
+                                                )}
                                             </Typography>
                                         )}
                                     </th>
@@ -174,8 +287,8 @@ export function InstructorPage() {
                         ))}
                     </thead>
                     <tbody className='bg-white'>
-                        {mockData &&
-                            mockData.length > 0 &&
+                        {listInstructorCourses &&
+                            listInstructorCourses.length > 0 &&
                             fileTable.getRowModel().rows.map((row, index) => {
                                 const isLast = index === fileTable.getRowModel().rows.length - 1;
                                 const classes = isLast ? 'p-4' : 'p-4 border-b border-blue-gray-50';
--- frontend/states/review.state.ts
+++ frontend/states/review.state.ts
@@ -6,52 +6,72 @@ export const useReviewStore = create<ReviewStore>()((set, get) => ({
     courseStatus: 'UNINIT',
     reviewData: [],
     listCourses: [],
+    listInstructorCourses: [],
     getReviewByStudentId: async (studentId) => {
-        set(() => ({ reviewStatus: 'PENDING' }));
+        set({ reviewStatus: 'PENDING' });
         try {
             const data = await reviewService.getByStudentId(studentId);
-            set(() => ({ reviewData: data, reviewStatus: 'SUCCESS' }));
+            set({ reviewData: data, reviewStatus: 'SUCCESS' });
         } catch (err) {
-            set(() => ({ reviewStatus: 'REJECT' }));
+            set({ reviewStatus: 'REJECT' });
         }
     },
     getListCoursesByStudentId: async (studentId) => {
-        set(() => ({ courseStatus: 'PENDING' }));
+        set({ courseStatus: 'PENDING' });
         try {
             const data = await reviewService.getListCoursesByStudentId(studentId);
-            set(() => ({ listCourses: data, courseStatus: 'SUCCESS' }));
+            set({ listCourses: data, courseStatus: 'SUCCESS' });
         } catch (err) {
-            set(() => ({ courseStatus: 'REJECT' }));
+            set({ courseStatus: 'REJECT' });
         }
     },
     createReview: async (payload) => {
-        set(() => ({ reviewStatus: 'PENDING' }));
+        set({ reviewStatus: 'PENDING' });
         try {
             await reviewService.create(payload);
+            await get().getListCoursesByStudentId(payload.studentId);
             await get().getReviewByStudentId(payload.studentId);
-            set(() => ({ reviewStatus: 'SUCCESS' }));
+            set({ reviewStatus: 'SUCCESS' });
         } catch (err) {
-            set(() => ({ reviewStatus: 'REJECT' }));
+            set({ reviewStatus: 'REJECT' });
         }
     },
     updateReview: async (payload) => {
-        set(() => ({ reviewStatus: 'PENDING' }));
+        set({ reviewStatus: 'PENDING' });
         try {
             await reviewService.update(payload);
             await get().getReviewByStudentId(payload.studentId);
-            set(() => ({ reviewStatus: 'SUCCESS' }));
+            set({ reviewStatus: 'SUCCESS' });
         } catch (err) {
-            set(() => ({ reviewStatus: 'REJECT' }));
+            set({ reviewStatus: 'REJECT' });
         }
     },
     deleteReview: async (studentId, courseId) => {
-        set(() => ({ reviewStatus: 'PENDING' }));
+        set({ reviewStatus: 'PENDING' });
         try {
             await reviewService.delete(studentId, courseId);
             await get().getReviewByStudentId(studentId);
-            set(() => ({ reviewStatus: 'SUCCESS' }));
+            set({ reviewStatus: 'SUCCESS' });
         } catch (err) {
-            set(() => ({ reviewStatus: 'REJECT' }));
+            set({ reviewStatus: 'REJECT' });
+        }
+    },
+    getListCoursesByInstructorId: async (instructorId) => {
+        set({ courseStatus: 'PENDING' });
+        try {
+            const data = await reviewService.getListCoursesByInstructorId(instructorId);
+            set({ listInstructorCourses: data, courseStatus: 'SUCCESS' });
+        } catch (err) {
+            set({ courseStatus: 'REJECT' });
+        }
+    },
+    filterAndSortCourses: async (instructorId, payload) => {
+        set({ courseStatus: 'PENDING' });
+        try {
+            const data = await reviewService.filterAndSortCourses(instructorId, payload);
+            set({ listInstructorCourses: data, courseStatus: 'SUCCESS' });
+        } catch (err) {
+            set({ courseStatus: 'REJECT' });
         }
     }
 }));
